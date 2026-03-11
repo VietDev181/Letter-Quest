c40edcca82f85a6d5428ba0e295d36dfc0422a80
VietDev181 <daohuyvietapp@gmail.com>
2026-03-11 09:10:08 +0700

    First Commit and BoardData

---
+++ Assets/Scripts/ScriptableObject/BoardData.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "BoardData", menuName = "ScriptableObjects/BoardData")]
+public class BoardData : ScriptableObject
+{
+    [System.Serializable]
+    public class SearchingWord
+    {
+        public string word;
+    }
+
+    [System.Serializable]
+    public class BoardRow
+    {
+        public int size;
+        public string[] row;
+
+        public BoardRow()
+        {
+            
+        }
+
+        public BoardRow(int size)
+        {
+            CreateRow(size);
+        }
+
+        public void CreateRow(int size)
+        {
+            this.size = size;
+            row = new string[size];
+        }
+
+        public void ClearRow()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                row[i] = " ";
+            }
+        }
+    }
+
+    public float timeInSeconds;
+    public int columns = 0;
+    public int rows = 0;
+
+    public BoardRow[] board;
+
+    public void ClearWithEmptyString()
+    {
+        for (int i = 0; i < columns; i++)
+        {
+            board[i].ClearRow();
+        }
+    }
+
+    public void CreateNewBoard()
+    {
+        board = new BoardRow[columns];
+        for (int i = 0; i < columns; i++)
+        {
+            board[i] = new BoardRow(rows);
+        }
+    }
+}
