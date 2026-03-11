191e73655116527254062668b3ea1c6b3b9975ad
VietDev181 <daohuyvietapp@gmail.com>
2026-03-11 16:57:01 +0700

    Create GameEvents

--- Assets/Scripts/Editor/AlphabetDataDrawer.cs
+++ Assets/Scripts/Editor/AlphabetDataDrawer.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
 
-public class AlphabetDataDrawer : MonoBehaviour
+[CustomEditor(typeof(AlphabetData))]
+[CanEditMultipleObjects]
+[System.Serializable]
+public class AlphabetDataDrawer : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    private ReorderableList alphabetPlainList;
+    private ReorderableList alphabetNormalList;
+    private ReorderableList alphabetHighlightedList;
+    private ReorderableList alphabetWrongList;
+
+    private void OnEnable()
     {
-        
+        InitializeReordableList(ref alphabetPlainList, "alphabetPlain", "Alphabet Plain");
+        InitializeReordableList(ref alphabetNormalList, "alphabetNormal", "Alphabet Normal");
+        InitializeReordableList(ref alphabetHighlightedList, "alphabetHighlighted", "Alphabet Highlighted");
+        InitializeReordableList(ref alphabetWrongList, "alphabetWrong", "Alphabet Wrong");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInspectorGUI()
     {
-        
+        serializedObject.Update();
+        alphabetPlainList.DoLayoutList();
+        alphabetNormalList.DoLayoutList();
+        alphabetHighlightedList.DoLayoutList();
+        alphabetWrongList.DoLayoutList();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void InitializeReordableList(ref ReorderableList list, string propertyName, string listLabel)
+    {
+        list = new ReorderableList(serializedObject, serializedObject.FindProperty(propertyName), true, true, true, true);
+        list.drawHeaderCallback = (Rect rect) =>
+        {
+            EditorGUI.LabelField(rect, listLabel);
+        };
+
+        var l = list;
+
+        list.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+        {
+            var element = l.serializedProperty.GetArrayElementAtIndex(index);
+            rect.y += 2;
+
+            EditorGUI.PropertyField(new Rect(rect.x, rect.y, 60, EditorGUIUtility.singleLineHeight), element.FindPropertyRelative("letter"), GUIContent.none);
+            EditorGUI.PropertyField(new Rect(rect.x + 70, rect.y, rect.width - 60 - 30, EditorGUIUtility.singleLineHeight), element.FindPropertyRelative("image"), GUIContent.none);
+        };
     }
 }
---
+++ Assets/Scripts/GridSquare.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSquare : MonoBehaviour
+{
+    public int SquareIndex {get; set; }
+
+    private AlphabetData.LetterData _normalLetterData;
+    private AlphabetData.LetterData _selectedLetterData;
+    private AlphabetData.LetterData _correctLetterData;
+
+    private SpriteRenderer _displayImage;
+
+    private bool _selected;
+    private bool _clicked;
+    private int _index = -1;
+
+    public void SetIndex(int index)
+    {
+        _index = index;
+    }
+
+    public int GetIndex()
+    {
+        return _index;
+    }
+    
+    void Start()
+    {
+        _selected = false;
+        _clicked = false;
+
+        _displayImage = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.OnEnableSquareSelection += OnEnableSquareSelection;
+        GameEvents.OnDisableSquareSelection += OnDisableSquareSelection;
+        GameEvents.OnSelectSquare += SelectSquare;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnEnableSquareSelection -= OnEnableSquareSelection;
+        GameEvents.OnDisableSquareSelection -= OnDisableSquareSelection;
+        GameEvents.OnSelectSquare -= SelectSquare;
+    }
+
+
+    public void OnEnableSquareSelection()
+    {
+        _clicked = true;
+        _selected = false;
+    }
+
+    public void OnDisableSquareSelection()
+    {
+        _clicked = false;
+        _selected = false;
+    }
+
+    public void SelectSquare(Vector3 position)
+    {
+        if (this.gameObject.transform.position == position)
+        {
+            _displayImage.sprite = _selectedLetterData.image;
+        }
+    }
+
+    public void SetSprite(AlphabetData.LetterData normalLetterData, 
+        AlphabetData.LetterData selectedLetterData, 
+        AlphabetData.LetterData correctLetterData)
+    {
+        _normalLetterData = normalLetterData;
+        _selectedLetterData = selectedLetterData;
+        _correctLetterData = correctLetterData;
+
+        GetComponent<SpriteRenderer>() .sprite = _normalLetterData.image;
+    }
+
+    void OnMouseDown()
+    {
+        OnEnableSquareSelection();
+        GameEvents.EnableSquareSelectionMethod();
+        CheckSquare();
+        _displayImage.sprite = _selectedLetterData.image;
+    }
+
+    void OnMouseEnter()
+    {
+        CheckSquare();
+    }
+
+    void OnMouseUp()
+    {
+        GameEvents.ClearSelectionMethod();
+        GameEvents.DisableSquareSelectionMethod();
+    }
+
+    public void CheckSquare()
+    {
+        if (_selected == false && _clicked == true)
+        {
+            _selected = true;
+            GameEvents.CheckSquareMethod(_normalLetterData.letter, gameObject.transform.position, _index);
+
+        }
+    }
+}
